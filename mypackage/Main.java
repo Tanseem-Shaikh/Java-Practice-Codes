c79df0f034953c3ea3540d89c20433ae1796885e
Tanseem-Shaikh <shaikhtanseem31@gmail.com>
2026-02-24 22:01:10 +0530

    Add files via upload

---
+++ mypackage/Main.java
@@ -0,0 +1,87 @@
+                                //GLOBAL VARIABLE
+package com.mypackage;
+public class Main {
+	static int emp_age=33;
+	static int emp_id=101;
+	static long emp_mobno=7645243389l;   
+	static String emp_name="Rohit";
+	static String emp_sal="5LPA";
+
+	public static void main(String args[]) {
+
+		System.out.println("Employee Age ="+emp_age);
+		System.out.println("Employee id ="+emp_id);
+		System.out.println("Employee Name ="+emp_name);
+		System.out.println("Employee Salary ="+emp_sal);
+		System.out.println("Employee Mobile-No ="+emp_mobno);
+	}
+}
+//public class Main{
+//	static byte age =21;
+//	static short id =101;
+//	static int Seat_no=234566;
+//	static long adhar_no=23455478092314l;
+//	static float marks=4.5f;
+//	static double Percentage=80.20;
+//		 
+//		 public static void main(String args[]) {
+//			 
+//		System.out.println("Student Age : "+age);
+//		System.out.println("Student id : "+id);
+//		System.out.println("Student Seat no : "+Seat_no);
+//		System.out.println("Student Adhar no : "+adhar_no);
+//		System.out.println("Student Marks : "+marks);
+//		System.out.println("Student Percentage : "+Percentage);
+//	 }
+// }
+
+//public class Main {
+//	static String Bank_name="Bank Of Maharashtra";
+//	static String branch="Latur";
+//	static String IFSC_code="MAHG00124";
+//	static String Account_Type="saving";
+//	static long Contact_Number=7534233477l;
+//	
+//	public static void main(String args[]) {
+//		
+//		System.out.println("Bank Name = " +Bank_name);
+//		System.out.println("Branch = " +branch );
+//		System.out.println("IFSC Code = " +IFSC_code);
+//		System.out.println("Account Type  = " +Account_Type);
+//		System.out.println("Contact Number = " +Contact_Number);
+//		
+//	}
+//}
+
+//public class Main {
+//	static String Vehicle_name="Scorpio";
+//	static String Vehicle_Type="Car";
+//	static String Vehicle_Number="MH12AB123";
+//	static String Vehicle_Color="Black";
+//	static String Vehicle_Price="3Lakh";
+//	public static void main(String args[]) {
+//		System.out.println("Vehicle name = " +Vehicle_name);
+//		System.out.println("Vehicle Type = " +  Vehicle_Type);
+//		System.out.println("Vehicle Number = " +Vehicle_Number );
+//		System.out.println("Vehicle Color = " + Vehicle_Color);
+//		System.out.println("Vehicle Price = " +Vehicle_Price );
+//		
+//		
+//	}
+//}
+//public class Main {
+//static String name="City Central Library";
+//static String address="Pune";
+//static String Time="10AM";
+//static int NoOfBooks=900;
+//static long contact_no=7534233477l;
+//public static void main(String args[]) {
+//System.out.println("Library Name = " +name);
+//System.out.println("Library Address = " +address);
+//System.out.println("Opening Time = " +Time);
+//System.out.println("Number of books = " +NoOfBooks);
+//System.out.println("Contact Number = " +contact_no);
+//
+//
+//}
+//}
