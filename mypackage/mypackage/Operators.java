adb10cd986ded94e0890d7994da709c8e77b2aee
Tanseem-Shaikh <shaikhtanseem31@gmail.com>
2026-02-26 17:31:20 +0530

    Add files via upload

---
+++ mypackage/mypackage/Operators.java
@@ -0,0 +1,17 @@
+package mypackage;
+
+public class Operators {
+	public static void main(String args[]) {
+		 int a=40;
+         System.out.println(a+20);
+         int b=20;
+         System.out.println(a+b);
+         int c=a+b;
+         System.out.println(c/a);
+         int d=c%2;
+         System.out.println(d+3);
+         System.out.println(d/c);
+	}
+       
+            
+}
