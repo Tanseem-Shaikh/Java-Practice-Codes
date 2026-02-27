3d5ee54312040dded284feb0cb499e6b5ce1342c
Tanseem-Shaikh <shaikhtanseem31@gmail.com>
2026-02-27 22:43:54 +0530

    Add files via upload

---
+++ mypackage/Operator/src/mypackage/ArithmaticOperator.java
@@ -0,0 +1,28 @@
+package mypackage;
+
+public class ArithmaticOperator {
+	public static void main(String args[]) {
+		int a=40;
+		int b=10;
+		int c=a+b;
+System.out.println("Addition of "+a + " + " + b + " = " +c);
+System.out.println("Substraction of "+a + " - " + b + " = " +(a-b));
+System.out.println("Multiplication of "+a + " * " + b + " = " +(a*b));
+System.out.println("Division of "+a + " / " + b + " = " +(a/b));
+System.out.println("Moduls of "+a + " % " + b + " = " +(a%b));
+
+System.out.println("------------------------");
+
+              // Practice
+
+int x=40;
+System.out.println(x+20);
+int y=20;
+System.out.println(x+y);
+int z=x+y;
+System.out.println(z/x);
+int d=z%2;
+System.out.println(d+3);
+System.out.println(d/z);
+	}
+}
