0296ec8ab594b8ca9303cdf0d3168331956d9525
Tanseem-Shaikh <shaikhtanseem31@gmail.com>
2026-03-06 12:39:41 +0530

    Add files via upload

---
+++ mypackage/IfElseStatements.java
@@ -0,0 +1,50 @@
+package mypackage;
+
+public class IfElseStatements {
+	public static void main(String args[]) {
+		//1 . Check Even or Odd Number
+		
+//		int number=18;
+//		if(number%2==0) {
+//			  System.out.println("number is even"); 
+//		}
+//		else {
+//			  System.out.println("number is odd"); 
+//		}
+//		//2. Check if a Person Can Vote
+//		
+//		int age=21;
+//		if(age>=18) {
+//			  System.out.println("Eligible for Voting"); 
+//		}
+//		else {
+//			  System.out.println(" Not Eligible for Voting"); 
+//		}
+//		//3 . Positive or Negative Number
+//		
+//		int num=4;
+//		if(num>0) {
+//			 System.out.println("Number is Psitive"); 
+//		}
+//		else if(num<0) {
+//			  System.out.println(" Number is negative"); 
+//		}
+//		else {
+//			 System.out.println(" Number is Zero"); 
+//		}
+		
+		//4 . Write a program to compare two numbers and print the greater one.
+		
+		int num1=100;
+		int num2=20;
+		if(num1>num2) {
+			System.out.println( "Greater Number is "+num1); 
+		}
+		else if(num2>num1) {
+			  System.out.println("Greater Number is "+num2); 
+		}
+		else {
+			 System.out.println(" Both Numbers are Equal"); 
+		}
+		}
+}
