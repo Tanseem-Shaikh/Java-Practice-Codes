4f84ea387a486d97110474df034f440ecea4c914
Tanseem-Shaikh <shaikhtanseem31@gmail.com>
2026-03-26 21:03:18 +0530

    Add files via upload

---
+++ mypackage/ConstructorExample.java
@@ -0,0 +1,57 @@
+package mypackage;
+//Constructor Example
+public class ConstructorExample{
+	public ConstructorExample() {
+		System.out.println("This is non Parametrized Constuctor");
+	}public ConstructorExample(String name){
+		System.out.println("This is Parametrized Constructor:"+name);
+	}
+}
+//   1.class with name myportfolio and display info using constructor
+//public class MyPortFolio {
+//public MyPortFolio(String name,String course,String college,int age) {
+//	System.out.println("My PortFolio");
+//	System.out.println("Name :"+name);
+//	System.out.println("Course :"+course);
+//	System.out.println("College :"+college);
+//	System.out.println("Age :"+age);
+//}
+//}
+
+//  2.class with name car and display info using constructor and add one method
+//public class Car{
+//	public Car(String brand,String Model,int Year,double price,String color) {
+//			System.out.println("Car Information");
+//			System.out.println("Brand :"+brand);
+//			System.out.println("Model :"+Model);
+//			System.out.println("Year :"+Year);
+//			System.out.println("Price :"+price);
+//			System.out.println("Color :"+color);
+//		} void display() {
+//			System.out.println("Car Details Displayed Successfully");
+//	}
+//}
+
+//   3.class with name Employee and display info using constructor and use salary method
+//public class Employee {
+//public Employee(String emp_name,String department ,int emp_id,double salary) {
+//	System.out.println("Employee information");
+//	System.out.println("Employee Name :"+emp_name);
+//	System.out.println("Department :"+department);
+//	System.out.println("Employee id :"+emp_id);
+//	System.out.println("Salary :"+salary);
+//}void Showsalary(double salary){
+//	System.out.println("Employee Salary is:"+salary);;
+//}
+//}
+
+//4.class with name student and display info using constructor 
+//public class Student{
+//public Student(String stu_name,String course ,int roll_no,double marks) {
+//System.out.println("Student information");
+//System.out.println("Name :"+stu_name);
+//System.out.println("Course :"+course);
+//System.out.println("Roll_No :"+roll_no);
+//System.out.println("Marks :"+marks);
+//}
+//}
