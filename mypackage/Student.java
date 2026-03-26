280f6bb7ee1555e1591799427a09d4604bbbf6ed
Tanseem-Shaikh <shaikhtanseem31@gmail.com>
2026-03-26 20:50:31 +0530

    Add files via upload

--- mypackage/Student.java
+++ mypackage/Student.java
@@ -1,19 +1,49 @@
-package com.mypackage;
+package mypackage;
+//   1.class with name myportfolio and display info using constructor
+//public class MyPortFolio {
+//public MyPortFolio(String name,String course,String college,int age) {
+//	System.out.println("My PortFolio");
+//	System.out.println("Name :"+name);
+//	System.out.println("Course :"+course);
+//	System.out.println("College :"+college);
+//	System.out.println("Age :"+age);
+//}
+//}
 
-public class Student {
- public static void main(String args[]) {
-	 byte age =21;
-	 short id =101;
-	 int Seat_no=234566;
-	 long adhar_no=23455478092314l;
-	 float marks=4.5f;
-	 double Percentage=80.20;
-	 System.out.println("Student Age : "+age);
-	 System.out.println("Student id : "+id);
-	 System.out.println("Student Seat no : "+Seat_no);
-	 System.out.println("Student Adhar no : "+adhar_no);
-	System.out.println("Student Marks : "+marks);
-	System.out.println("Student Percentage : "+Percentage);
-	
- }
+//  2.class with name car and display info using constructor and add one method
+//public class Car{
+//	public Car(String brand,String Model,int Year,double price,String color) {
+//			System.out.println("Car Information");
+//			System.out.println("Brand :"+brand);
+//			System.out.println("Model :"+Model);
+//			System.out.println("Year :"+Year);
+//			System.out.println("Price :"+price);
+//			System.out.println("Color :"+color);
+//		} void display() {
+//			System.out.println("Car Details Displayed Successfully");
+//	}
+//}
+
+//   3.class with name Employee and display info using constructor and use salary method
+//public class Employee {
+//public Employee(String emp_name,String department ,int emp_id,double salary) {
+//	System.out.println("Employee information");
+//	System.out.println("Employee Name :"+emp_name);
+//	System.out.println("Department :"+department);
+//	System.out.println("Employee id :"+emp_id);
+//	System.out.println("Salary :"+salary);
+//}void Showsalary(double salary){
+//	System.out.println("Employee Salary is:"+salary);;
+//}
+//}
+
+//4.class with name student and display info using constructor 
+public class Student{
+public Student(String stu_name,String course ,int roll_no,double marks) {
+System.out.println("Student information");
+System.out.println("Name :"+stu_name);
+System.out.println("Course :"+course);
+System.out.println("Roll_No :"+roll_no);
+System.out.println("Marks :"+marks);
+}
 }
