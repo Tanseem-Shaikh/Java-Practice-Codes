a4dfdf5f8cf6e35e51ac7096a97c8c254fea25c6
Tanseem-Shaikh <shaikhtanseem31@gmail.com>
2026-04-26 19:28:05 +0530

    Add files via upload

---
+++ DataTypes/Library.java
@@ -0,0 +1,18 @@
+package com.mypackage;
+
+public class Library {
+	
+		static String name="City Central Library";
+		static String address="Pune";
+		static String Time="10AM";
+		static int NoOfBooks=900;
+		static long contact_no=7534233477l;
+		public static void main(String args[]) {
+		System.out.println("Library Name = " +name);
+		System.out.println("Library Address = " +address);
+		System.out.println("Opening Time = " +Time);
+		System.out.println("Number of books = " +NoOfBooks);
+		System.out.println("Contact Number = " +contact_no);
+		
+	}
+}
---
+++ DataTypes/Vehicle.java
@@ -0,0 +1,13 @@
+package com.mypackage;
+
+public class Vehicle {
+	public static void main(String args[]) {
+		System.out.println("Vehicle name = " + "Scorpio");
+		System.out.println("Vehicle Type = " + "Car");
+		System.out.println("Vehicle Number = " + "MH12AB123");
+		System.out.println("Vehicle Color = " + "Black");
+		System.out.println("Vehicle Price = " + "3lakh");
+		
+		
+	}
+}
